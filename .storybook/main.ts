b922db4aa2a93544cf3cb557fde4c95fdd6df379
ahmadadnanes <aa264048@gmail.com>
2025-07-22 15:08:14 +0300

    Make Vartheme BS5 Components the Default and Remove Varbase Components #5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,7 @@ import type { StorybookConfig } from '@storybook/server-webpack5';
 const config: StorybookConfig = {
   // Change the place where storybook searched for stories.
   "stories": [
-    // Listing Varbase Components in the Storybook
-    "../docroot/modules/contrib/varbase_components/components/**/*.mdx",
-    "../docroot/modules/contrib/varbase_components/components/**/*.stories.@(json)",
-    // -------------------------------------------------------------------------------
-    // Listing components Vartheme BS5 Starterkit. ( Comment when using a custom theme for a project)
+    // Listing components from Vartheme BS5 Starterkit. ( Comment when using a custom theme for a project)
     "../docroot/themes/contrib/vartheme_bs5/components/**/*.mdx",
     "../docroot/themes/contrib/vartheme_bs5/components/**/*.stories.@(json)",
     // -------------------------------------------------------------------------------
@@ -15,10 +11,6 @@ const config: StorybookConfig = {
     // Change `mytheme` to the name of the custom theme.
     // "../docroot/themes/custom/mytheme/components/**/*.mdx",
     // "../docroot/themes/custom/mytheme/components/**/*.stories.@(json)",
-    // -------------------------------------------------------------------------------
-    // Uncomment the following line to start listing components from custom modules
-    // "../docroot/modules/custom/my_custom_module/components/**/*.mdx",
-    // "../docroot/modules/custom/my_custom_module/components/**/*.stories.@(json)",
   ],
   "addons": [
     "@storybook/addon-links",
